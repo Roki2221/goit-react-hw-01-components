d344fa248e7ea1e29a794b4bb2b53bb3febb1761
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-12-03 13:22:52 +0200

    markup first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import Profile from './Profile/index';
+import data from '/git/goit-react-hw-01-components/src/user.json';
 export const App = () => {
   return (
     <div
@@ -7,10 +9,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={data.username}
+        tag={data.tag}
+        location={data.location}
+        avatar={data.avatar}
+        stats={data.stats}
+      />
     </div>
   );
 };
